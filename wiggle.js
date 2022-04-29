7565f0aa326c02bb1b157e184acb4f243d1fc176
JOBLIN <48665034+BrownBearDude@users.noreply.github.com>
2022-04-29 15:59:10 +1000

    did some html

--- wiggle.js
+++ wiggle.js
@@ -1,29 +1,38 @@
 
 function char_effic_str(offset) {
     let string = "";
-    string += "U+0009".repeat(Math.floor(offset / 8));
+    string += "	".repeat(Math.floor(offset / 8));
     string += " ".repeat(offset % 8);
     let length = Math.floor(offset / 8) + (offset % 8)
     return {string, length}
 }
 
+function good_mod(m, n) {
+    return ((m % n) + n) % n
+}
 
 function main() {
     let output = ""
     let looping = true
     let char_count = 0
+    let text = "wiggle"
     let i = 0
     while (looping) {
-        let o = Math.floor((Math.sin(i) + 1) * 10);
+        let o = Math.floor((Math.sin(i)+1)*50);
+
+
+
+        
         let eff = char_effic_str(o);
         console.log(eff)
-        if (char_count + eff.length + 7 < 2000) {
-            output += eff.string + "wiggle" + "\n";
+        if (char_count + eff.length +  text.length + 1 < 2000) {
+            
+            output += eff.string + text + "\n";
             char_count += eff.length + 8; 
         } else {
             looping = false
         }
-        i += Math.PI * 2 /25
+        i += Math.PI * 2 / 25
     }
     console.log(output)
     console.log(char_count)
@@ -31,5 +40,3 @@ function main() {
 }
 
 main()
-
-
---
+++ wiggle2.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("form");
+
+const submit = function (e) {
+    e.preventDefault()
+    console.log("bruh")
+
+    let formula = form["formula"].value
+
+    let ouput = ""
+    let depth = 50
+    let line = 0
+    let looping = true
+    while (looping) {
+
+        if 
+    }
+
+    eval(formula)
+
+
+
+}
+
+
+form.addEventListener("submit", submit, event)
